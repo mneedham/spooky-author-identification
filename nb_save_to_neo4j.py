72375dea5bdfedf334c8ee5f76a41d48e869850b
Mark Needham <mark.needham@neotechnology.com>
2018-04-01 13:17:12 +0100

    save simple values too

--- nb_save_to_neo4j.py
+++ nb_save_to_neo4j.py
@@ -1,16 +1,16 @@
-import importlib
-import numpy as np
 import datetime
+import importlib
 
-from sklearn import datasets
-from sklearn.naive_bayes import GaussianNB
-from sklearn import linear_model
-
+import numpy as np
 from neo4j.v1 import GraphDatabase, basic_auth
+from sklearn import datasets
+from sklearn import linear_model
 
 
 # save the binary representation and then load it back using reshape
 #  np.frombuffer(raw_value.tobytes()).reshape(raw_value.shape)
+from sklearn.naive_bayes import GaussianNB
+
 
 class ModelPersistence:
     def __init__(self, driver):
@@ -41,19 +41,21 @@ class ModelPersistence:
         attrs = params["fields"]
         for field in fields:
             raw_value = getattr(model, field)
-            print(field, raw_value)
             coerced_value = raw_value
 
-            # print(type(value).__module__, type(value).__name__)
             if isinstance(raw_value, np.ndarray):
                 coerced_value = raw_value.tobytes()
 
+            data_type = "{0}.{1}".format(type(raw_value).__module__, type(raw_value).__name__)
+
             attrs[field] = {
                 "value": coerced_value,
-                "type": "{0}.{1}".format(type(raw_value).__module__, type(raw_value).__name__),
-                "dataType": raw_value.dtype.name,
-                "shape": list(raw_value.shape)
+                "type": data_type
             }
+
+            if data_type == "numpy.ndarray":
+                attrs[field]["dataType"] = raw_value.dtype.name
+                attrs[field]["shape"] = list(raw_value.shape)
 
         with self.driver.session() as session:
             session.run(self.save_model_query, params)
@@ -73,12 +75,12 @@ class ModelPersistence:
         ModelClass = getattr(importlib.import_module(model["module"]), model["class"])
         instance = ModelClass()
         for field in fields:
-            value = np.frombuffer(field["value"], dtype=field["dataType"]).reshape(tuple(field["shape"]))
-            print(field["key"], value)
-            # value = model["fields"][attr]["value"]
+            if field["type"] == "numpy.ndarray":
+                value = np.frombuffer(field["value"], dtype=field["dataType"]).reshape(tuple(field["shape"]))
+            else:
+                value = field["value"]
             setattr(instance, field["key"], value)
         return instance
-
 
 
 persister = ModelPersistence(GraphDatabase.driver("bolt://localhost", auth=basic_auth("neo4j", "neo")))
@@ -90,12 +92,8 @@ iris = datasets.load_iris()
 gnb = GaussianNB()
 gnb.fit(iris.data, iris.target)
 
-
-
-model_name = "blahblash-{0}".format(datetime.datetime.timestamp(datetime.datetime.now()))
-
+model_name = "{0}-{1}".format(gnb.__class__.__name__, int(datetime.datetime.timestamp(datetime.datetime.now())))
 persister.save(gnb, model_name, ["classes_", "sigma_", "theta_", "class_prior_"])
-
 
 y_pred = gnb.predict(iris.data)
 print("Number of mislabeled points out of a total %d points : %d" % (iris.data.shape[0], (iris.target != y_pred).sum()))
@@ -107,13 +105,14 @@ print("Number of mislabeled points out of a total %d points : %d" % (iris.data.s
 
 # Lasso
 
-# reg = linear_model.Lasso(alpha=0.1)
-# reg.fit([[0, 0], [1, 1]], [0, 1])
-#
-# saved_model = save_model(reg, ["coef_", "n_iter_", "intercept_"])
-#
-# print(reg.predict([[1, 1]]))
-#
-# new_reg = load_model(saved_model)
-#
-# print(new_reg.predict([[1, 1]]))
+reg = linear_model.Lasso(alpha=0.1)
+reg.fit([[0, 0], [1, 1]], [0, 1])
+
+model_name = "{0}-{1}".format(reg.__class__.__name__, int(datetime.datetime.timestamp(datetime.datetime.now())))
+persister.save(reg, model_name, ["coef_", "n_iter_", "intercept_"])
+
+print(reg.predict([[1, 1]]))
+
+new_reg = persister.load(model_name)
+
+print(new_reg.predict([[1, 1]]))
